5837edc4da4920870eca3dab62efc8fc3af7bd93
Carlos <carloscamposhs@gmail.com>
2015-05-29 16:33:01 -0300

    Inicial

---
+++ Arquitetura/src/classes/MemoriaDados.java
@@ -0,0 +1,31 @@
+package classes;
+
+public class MemoriaDados {
+	private byte[] dadosNaMemoria = new byte[4096];
+	
+
+	public void escreveNaMemoria(int pos, byte dado) {
+		if(pos<0 || pos>4093){
+			System.out.println(pos);
+			System.out.println("Overflow");
+		}
+		if(pos>=2048 && pos<3935){
+			System.out.println("Memoria inacessivel");
+		}else{
+			
+		dadosNaMemoria[pos]=dado;
+		}
+	}
+
+	public byte getDadosnaMemoria(int pos) {
+		if(pos>=2048 && pos<3935){
+			System.out.println("Memória inacessivel");
+			return 0;
+			
+		}else{
+			return this.dadosNaMemoria[pos];
+		}
+	}
+
+
+}
---
+++ Arquitetura/src/classes/Teste.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MemoriaPrograma a = new MemoriaPrograma();
+		a.escrever(10, 65535);
+		a.lerInstrucao(10);
+	}
+
+}
